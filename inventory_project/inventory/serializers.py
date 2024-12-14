82027ca10ba89679886b874440725c45a2c78c92
mahama saani Zakaria <zakariamahamasaani@gmail.com>
2024-12-14 15:39:47 +0000

    Implemented core endpoints and tested functionality

---
+++ inventory_project/inventory/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from .models import User, Category, Supplier, Product, Order, OrderItem
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "role"]
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+
+class SupplierSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Supplier
+        fields = "__all__"
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = "__all__"
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True)
+
+    class Meta:
+        model = Order
+        fields = "__all__"
--- inventory_project/inventory/views.py
+++ inventory_project/inventory/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Category, Supplier, Product, Order, OrderItem
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.views import TokenVerifyView
 
-# Create your views here.
+from .serializers import (
+    CategorySerializer,
+    SupplierSerializer,
+    ProductSerializer,
+    OrderSerializer,
+)
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class SupplierViewSet(viewsets.ModelViewSet):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
